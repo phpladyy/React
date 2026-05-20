11540257828843a15c5129ed8cf01ce5551b7504
phpladyy <eluwina08325@gmail.com>
2026-05-20 15:33:06 +0200

    added first project

---
+++ firstSteps/advice-app/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function App() {
+
+  const [advice, setAdvice] = useState("");
+  const [joke, setJoke] = useState(null);
+  async function GetAdvice() {
+          const getadvice = await fetch("https://api.adviceslip.com/advice");
+          const data = await getadvice.json();
+          setAdvice(data.slip.advice);
+          
+        }
+  async function getJoke() {
+    const generateJoke = await fetch("https://v2.jokeapi.dev/joke/Any");
+    const processJoke = await generateJoke.json();
+
+    setJoke(processJoke);
+  }
+  return (
+    <div>
+      <h1>Advice generator</h1>
+      <button onClick={GetAdvice}>Click for advice</button> <button onClick={getJoke}>Click for a  joke</button>
+      <h2>{`Advice: ${advice}`}</h2>
+        <h2> 
+         {`Joke: ${joke?.setup ?? joke?.joke ?? ""} ${joke?.delivery ?? ""}`}
+        </h2>
+    </div>
+  );
+}
+
+export default App;
