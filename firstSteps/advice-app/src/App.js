d8e44a17ec8b6906b3908df01c53548c5e7bac2e
phpladyy <eluwina08325@gmail.com>
2026-05-20 16:34:34 +0200

    advice app finished

--- firstSteps/advice-app/src/App.js
+++ firstSteps/advice-app/src/App.js
@@ -1,32 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './Styles.css';
 
 function App() {
-
+  const [adviceCounter, setAdviceCounter] = useState(0);
   const [advice, setAdvice] = useState("");
   const [joke, setJoke] = useState(null);
   async function GetAdvice() {
           const getadvice = await fetch("https://api.adviceslip.com/advice");
           const data = await getadvice.json();
           setAdvice(data.slip.advice);
-          
+          setAdviceCounter((counter)=> counter+1);
         }
-  async function getJoke() {
+  async function GetJoke() {
     const generateJoke = await fetch("https://v2.jokeapi.dev/joke/Any");
     const processJoke = await generateJoke.json();
-
     setJoke(processJoke);
   }
+
+ 
+
   return (
     <div>
       <h1>Advice generator</h1>
-      <button onClick={GetAdvice}>Click for advice</button> <button onClick={getJoke}>Click for a  joke</button>
+      <button onClick={GetAdvice}>Click for advice</button> <button onClick={GetJoke}>Click for a  joke</button>
       <h2>{`Advice: ${advice}`}</h2>
         <h2> 
          {`Joke: ${joke?.setup ?? joke?.joke ?? ""} ${joke?.delivery ?? ""}`}
         </h2>
+        <Counter count = {adviceCounter} />
     </div>
   );
+}
+
+function Counter(props){
+  return(
+    <h3>{`Asked for advice ${props.count} time(s)`}</h3>
+);
 }
 
 export default App;
