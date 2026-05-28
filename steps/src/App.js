73107177df0da66e573873b523995f5ccb7e301d
phpladyy <eluwina08325@gmail.com>
2026-05-28 19:10:41 +0200

    finished majority of app

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,7 +1,41 @@
-function App() {
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  
+  const handlePreviousClick = () => step > 1 && setStep(step - 1);
+  const handleNextClick = () => step < 3 && setStep(step + 1);
+
   return (
-    <div>Test</div>
+    <div className="steps">
+      <div className="numbers">
+        <div className={step === 1 ? "active" : "non-active"}>1</div>
+        <div className={step === 2 ? "active" : "non-active"}>2</div>
+        <div className={step === 3 ? "active" : "non-active"}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          onClick={handlePreviousClick}
+          style={{ backgroundColor: "purple", color: "white" }}
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleNextClick}
+          style={{ backgroundColor: "purple", color: "white" }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
   );
 }
-
-export default App;
