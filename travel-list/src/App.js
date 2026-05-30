8a69ddb180ac63438cd21d0f9f14a3522e93b20a
phpladyy <eluwina08325@gmail.com>
2026-05-30 15:14:12 +0200

    added list rendering

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,4 +1,11 @@
 import "./index.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "charger", quantity: 1, packed: false },
+];
+
 function App() {
   return (
     <div className="app">
@@ -23,7 +30,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={{textDecoration: item.packed ? 'line-through':''}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
