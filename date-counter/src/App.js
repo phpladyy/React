da08336538ce52db3dff8969ac0ee7af8a58403d
phpladyy <eluwina08325@gmail.com>
2026-05-31 12:31:52 +0200

    upgraded dateCounter gui

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../src/styles.css"
+import "../src/styles.css";
 
 function App() {
   return (
@@ -10,39 +10,67 @@ function App() {
 }
 
 function Controls() {
-  const [steps, setSteps] = useState(0);
-  const [count, setCount] = useState(0);
+  const defaultSteps = 1;
+  const defaultCount = 0;
+  const [steps, setSteps] = useState(defaultSteps);
+  const [count, setCount] = useState(defaultCount);
+
+  function resetValues() {
+    setSteps(defaultSteps);
+    setCount(defaultCount);
+  }
+
   return (
     <div>
       <h2>Date counting tool by foxie</h2>
       <div className="controls">
-        <button onClick={() => steps>0 && setSteps((s) => s - 1)}>-</button>
-        Step: {steps}
-        <button onClick={() => setSteps((s) => s + 1)}>+</button>
+        <input
+          type="range"
+          onChange={(e) => setSteps(Number(e.target.value))}
+          value={steps}
+          min={0}
+          max={10}
+        />
+        {steps}
       </div>
       <div className="controls">
         <button onClick={() => setCount((s) => s - steps)}>-</button>
-        Count: {count}
+        <input className="textInput"
+          type="text"
+          onChange={(e) =>
+            e.target.value !== "-"
+              ? setCount(Number(e.target.value))
+              : setCount('-')
+          }
+          value={count !==0 ? count:''}
+        />
+
         <button onClick={() => setCount((s) => s + steps)}>+</button>
       </div>
-      <ShowDate />
+      <ShowDate count={count} />
+      {count === defaultCount && steps === defaultSteps ? (
+        ""
+      ) : (
+        <button onClick={resetValues} className="reset">
+          RESET
+        </button>
+      )}
     </div>
   );
-
-  function ShowDate() {
-    const curDate = new Date();
-    curDate.setDate(curDate.getDate() + count);
-
-    return (
-        <div>
-          {curDate > Date.now()
-            ? `${count} Days until: ${curDate.toDateString()}`
-            : curDate < Date.now()
-              ? `${Math.abs(count)} Days ago was: ${curDate.toDateString()}`
-              : `Today is ${curDate.toDateString()}`}
-        </div>
-    );
-  }
 }
 
+function ShowDate({ count }) {
+  const curDate = new Date();
+  curDate.setDate(curDate.getDate() + count);
+
+  return (
+    <div>
+      {curDate > Date.now()
+        ? `${count} Days until: ${curDate.toDateString()}`
+        : curDate < Date.now()
+          ? `${Math.abs(count)} Days ago was: ${curDate.toDateString()}`
+          : `Today is ${curDate.toDateString()}`}
+    </div>
+  );
+}
 export default App;
