fb04010b7a3fde2bf3b348eca3d5690809d9c623
phpladyy <eluwina08325@gmail.com>
2026-06-03 14:42:42 +0200

    created accordion list project

---
+++ accordion/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./styles.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
+}
+
+function Accordion() {
+  return (
+    <div className="accordion">
+      {faqs.map((question, index) => (
+        <Element key={question.title} index={index} title={question.title} text={question.text} />
+      ))}
+    </div>
+  );
+}
+
+function Element({ title, text, index }) {
+  function handleToggle() {
+    setIsOpen(!isOpen);
+  }
+
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div onClick={handleToggle} className={`item ${isOpen ? 'open':' '}`}>
+      <p className="number">
+        {index < 10 && "0"}
+        {index + 1}
+      </p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
