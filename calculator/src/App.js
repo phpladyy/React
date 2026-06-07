d43a62b98fe78382e6f04ab1a569f6047516cbba
phpladyy <eluwina08325@gmail.com>
2026-06-07 14:38:22 +0200

    fixed logic problems

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { evaluate, number } from "mathjs";
+import { evaluate } from "mathjs";
 
 function App() {
   const [firstNumber, setEquation] = useState("");
