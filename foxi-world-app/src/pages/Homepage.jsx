821b57a16a11ac263a66dd23a84a8f4cfb8abf6b
phpladyy <eluwina08325@gmail.com>
2026-07-27 16:02:28 +0200

    css modules added

--- foxi-world-app/src/pages/Homepage.jsx
+++ foxi-world-app/src/pages/Homepage.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-    return (
-        <div>
-            WorldWise
-        </div>
-    )
+  return (
+    <div>
+      <PageNav />
+
+      <h1>WorldWise</h1>
+      <Link to={"/app"}>Go to the aapp</Link>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
--- foxi-world-app/src/pages/PageNotFound.jsx
+++ foxi-world-app/src/pages/PageNotFound.jsx
@@ -1,6 +1,9 @@
+import PageNav from "../components/PageNav"
+
 function PageNotFound() {
     return (
         <div>
+            <PageNav/>
             Not found
         </div>
     )
